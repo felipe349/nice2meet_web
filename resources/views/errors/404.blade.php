949a44bc6b27e1eab3b8913df47700bcf397d628
barbaromatrix <matheus.galdino@hotmail.com>
2017-05-31 19:56:32 +0000

    Corrigindo problemas no Quiz na hospedagem

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta name="viewport" content="width=device-width" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+	    <!-- color url bar -->
+	    <meta name="theme-color" content="#659fe7" />
+		<title>Nice2Meet</title>
+		<meta charset="UTF-8">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<style type="text/css">
+			.mensagem404 {
+				margin-top: 200px;
+				width: 240px;
+				color: #6c6b6b;
+			    font-family: Lato,sans-serif;
+			    font-size: .875em;
+			    font-weight: normal;
+			    text-decoration: none;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			
+			.link--black {
+				text-decoration: none;
+				color: black;
+			}
+		</style>
+	</head>
+
+	<body>
+		<figure class="figure left"> 
+			<div class='mensagem404'>
+				<a href="/">
+					<i class="fa fa-exclamation-triangle" style="font-size:14rem; color: #f37021" aria-hidden="true"></i>
+				</a>
+				
+				<h3 class="grey" style="text-align: center">
+					 A página que você tentou acessar não existe :(
+				</h3>
+				<h3 class="grey" style="text-align: center">
+					<a class="link--black" href="/">Caso queira voltar, é só clicar aqui.</a>
+				</h3>
+			</div>
+
+		</figure>
+
+	</body>
+
+</html>
