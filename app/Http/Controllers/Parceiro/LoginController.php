1bbc6ca9a18325f1257a8446643f73882619b93a
Felipe <claiohm@gmail.com>
2017-08-26 20:01:46 +0000

    Checar se banco esta rodando

--- app/Http/Controllers/Parceiro/LoginController.php
+++ app/Http/Controllers/Parceiro/LoginController.php
@@ -18,7 +18,7 @@ class LoginController extends Controller
     
     public function makeLogin(\App\Http\Requests\LoginRequest $request)
     {
-        if (Auth::guard('parceiro')->attempt(['nm_email_parceiro' => $request->input('email')])) {
+        if (Auth::guard('parceiro')->attempt(['nm_email_parceiro' => $request->input('email'), 'nm_senha_parceiro' => $request->input('password')])) {
             // Authentication passed...
             return redirect()->intended('Parceiro/');
             
