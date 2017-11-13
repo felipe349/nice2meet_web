901d3aa56fa9ea1754eaa46abd0e56d2373aef0f
Felipe <claiohm@gmail.com>
2017-11-13 15:26:42 +0000

    removida data inicial do cupom

--- app/Models/Cupom.php
+++ app/Models/Cupom.php
@@ -10,7 +10,7 @@ class Cupom extends Model
     protected $table = 'tb_cupom';
     protected $primaryKey = 'id_cupom';
     protected $fillable = [
-        'id_oferta_turista', 'dt_inicial_cupom', 'dt_final_cupom', 'cd_cupom', 'ic_status', 'ic_validado'
+        'id_oferta_turista', 'dt_final_cupom', 'cd_cupom', 'ic_status', 'ic_validado'
     ];
     
     protected $dates = ['dt_inicial_cupom', 'dt_final_cupom'];
